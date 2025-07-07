84eeb5967c55ba0425687d2803fcf052e8104aeb
Niraja85 <tripathi.niraja@gmail.com>
2025-07-08 01:22:55 +0200

    Added views

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -16,16 +15,11 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    field_2 = models.Integer(default= 42)
-    field_3 = models.CharField(null=True)
 
     class Meta:
-        ordering = ["created_on"]
-    def __str__(self):
-        return f"The title of this post is {self.title}"
+        ordering = ["-created_on"]
     def __str__(self):
         return f"{self.title} | written by {self.author}"
-
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -35,8 +29,10 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
+
